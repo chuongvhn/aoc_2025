a9b60d74f8af86abba493ca28b9565775ae53639
chuongvhn <chuongvhn>
2025-12-12 21:11:44 +0000

    wip

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -33,15 +33,84 @@ fn get_battery_ids(line: &String) -> u32 {
         }
     }
 
-    let mut ret = 0;
     if max_0_idx < max_1_idx {
-        ret = max_0 * 10 + max_1;
+        max_0 * 10 + max_1
     }
     else {
-        ret = max_1 * 10 + max_0;
+        max_1 * 10 + max_0
     }
+}
+
+struct MaxDigitAndPos {
+    digit : u32,
+    pos : usize
+}
+
+fn get_max_digit_and_its_pos(input: &mut VecDeque<u32>, digit_and_pos : MaxDigitAndPos) -> MaxDigitAndPos{
+    let mut ret : MaxDigitAndPos = MaxDigitAndPos { digit: 0, pos: 0 };
+    let actuall_pos : usize;
+    if digit_and_pos.pos == 0 && digit_and_pos.digit ==0 {
+        actuall_pos = 0;
+    }
+    else {
+        actuall_pos = digit_and_pos.pos +1;
+    }
+
+    if actuall_pos != input.len() -1 {
+        for idx in actuall_pos..input.len() as usize {
+            if ret.digit < input[idx] {
+                ret.digit = input[idx];
+                ret.pos = idx;
+            }
+        }
+    }
+    else {
+        panic!("something wrong");
+    }
+    
     return ret;
 }
+
+fn get_battery_ids_part2(input: &String)-> u64 {
+    let mut result : u64 = 0;
+    let mut numbers: VecDeque<u32> = VecDeque::new();
+    for s in input.chars() {
+        numbers.push_back(s.to_string().parse().unwrap());
+    }
+    const NUM_OF_DIGITS : u32 = 12;
+
+    let mut max_digit_and_its_pos = MaxDigitAndPos{digit: 0, pos: 0 };
+    for i in 0..NUM_OF_DIGITS {
+        max_digit_and_its_pos = get_max_digit_and_its_pos(&mut numbers,max_digit_and_its_pos);
+        if max_digit_and_its_pos.pos == numbers.len() - 1 {
+            result += max_digit_and_its_pos.pos as u64;
+        }
+        else {
+            result += max_digit_and_its_pos.digit as u64 * 10u64.pow(NUM_OF_DIGITS -i -1);
+        }
+    }
+
+    return result;
+}
+
+#[test]
+fn test_get_battery_ids_part2() {
+    let line = "987654321111111".to_string();
+    assert_eq!(987654321111, get_battery_ids_part2(&line));
+    let line = "811111111111119".to_string();
+    assert_eq!(811111111119, get_battery_ids_part2(&line));
+    let line = "234234234234278".to_string();
+    assert_eq!(434234234278, get_battery_ids_part2(&line));
+    let line = "818181911112111".to_string();
+    assert_eq!(888911112111, get_battery_ids_part2(&line));
+    // let line = "2222422126525122433332324122232442621332112124353325142213221221321242522245242222212213223253222222".to_string();
+    // assert_eq!(66,get_battery_ids_part2(&line));
+    // let line = "6986637616744837475696535678356366536287839555455376737334329457284565566954578742689469447785687752".to_string();
+    // assert_eq!(99, get_battery_ids_part2(&line));
+    // let line = "4444355567375444144441434742443725566463147643443444453733475443534453656464433364465467345427344384".to_string();
+    // assert_eq!(84, get_battery_ids_part2(&line));
+}
+
 
 #[test]
 fn test_get_battery_ids() {
@@ -64,11 +133,19 @@ fn test_get_battery_ids() {
 fn main() {
     let input = aoc_2025::read_file("input_day03.txt");
     let mut result : u32 = 0;
-    for line in input {
+    for line in &input {
         let ret = get_battery_ids(&line);
         println!("{line} : {ret}");
         result += ret;
 
     }
-    println!("{result}");
+
+    let mut result2: u64 = 0;
+    for line in input {
+        let ret = get_battery_ids_part2(&line);
+        println!("{line} : {ret}");
+        result2 += ret;
+
+    }
+    println!("{result} : {result2}");
 }
