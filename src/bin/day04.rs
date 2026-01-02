7387735010d31595776acd1807892d921ce838af
Chuong Vo <chuong.vhn@gmail.com>
2026-01-02 21:56:47 +0000

    completed day 04 part 2

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -29,6 +29,59 @@ fn part1(buffer: &[Vec<u32>; 140]) -> u32 {
     cnt
 }
 
+struct NumberOfRollsAndPosition {
+    cnt: u32,
+    position: Vec<(usize, usize)>
+}
+
+fn check_adjacent_pos_for_part2(buffer: &mut [Vec<u32>; 140]) -> NumberOfRollsAndPosition {
+    let mut cnt = 0u32;
+    let mut pos_can_be_removed: Vec<(usize, usize)> = Vec::new();
+    for row_idx in 0..buffer.len() {
+        for col_idx in 0..buffer[row_idx].len() {
+            let eight_adjacent_pos = [
+                (-1isize, -1isize), (-1, 0), (-1, 1),
+                (0, -1),                    (0, 1),
+                (1, -1),  (1, 0),  (1, 1),
+            ];
+            if buffer[row_idx][col_idx] == 1 {
+                let mut numer_of_pos_has_value = 0;
+                for &pos in &eight_adjacent_pos {
+                    let new_row = row_idx as isize + pos.0;
+                    let new_col = col_idx as isize + pos.1;
+                    if new_row >= 0 && (new_row as usize) < buffer.len() &&
+                        new_col >= 0 && (new_col as usize) < buffer[row_idx].len() &&
+                        buffer[new_row as usize][new_col as usize] == 1 {
+                        numer_of_pos_has_value += 1;
+                    }
+                }
+                if numer_of_pos_has_value < 4 {
+                    pos_can_be_removed.push((row_idx as usize,col_idx as usize));
+                    cnt += 1;
+                }
+            }
+            
+        }
+    }
+    NumberOfRollsAndPosition{cnt: cnt, position: pos_can_be_removed }
+}
+
+fn part2(buffer: &mut [Vec<u32>;140]) -> u32 {
+    let mut result = 0;
+    loop {
+        let ret = check_adjacent_pos_for_part2(buffer);
+        if ret.cnt == 0 {
+            break;
+        }
+        else {
+            for pos in ret.position {
+                buffer[pos.0][pos.1] = 0;
+            }
+        }
+        result += ret.cnt;
+    }
+    result
+}
 
 fn main() {
     let input = aoc_2025::read_file("input_day04.txt");
@@ -48,4 +101,6 @@ fn main() {
     }
     let result = part1(&buffer);
     println!("Part 1: {}", result);
+    let result =part2(&mut buffer);
+    println!("Part 2: {}", result);
 }
