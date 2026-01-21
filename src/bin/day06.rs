f1a4459366801df7e08d476cc73136650f4d73e3
chuongvhn <chuongvhn>
2026-01-21 15:27:15 +0000

    wip

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -25,8 +25,43 @@ fn parse_input_to_work_sheet(input: &[String]) -> u64 {
     result
 }
 
+fn part2(input: &[String]) -> u64 {
+    let mut result = 0u64;
+    let mut work_sheet :Vec<Vec<char>> = Vec::new();
+    for (row_idx, line) in input.iter().enumerate() {
+        work_sheet.push(Vec::new());
+        for c in line.chars() {
+            work_sheet[row_idx].push(c);
+        }
+        result += 1;
+    }
+
+    let mut parsed_number_from_worksheet : Vec<String> = Vec::new();
+    for idx in (0..work_sheet[0].len() -1).rev() {
+        if work_sheet[0][idx] != ' ' && work_sheet[1][idx] != ' ' && work_sheet[2][idx] != ' ' && work_sheet[3][idx] != ' ' {
+            let mut num : String = String::new();
+            num.push(work_sheet[0][idx]);
+            num.push(work_sheet[1][idx]);
+            num.push(work_sheet[2][idx]);
+            num.push(work_sheet[3][idx]);
+            parsed_number_from_worksheet.push(num);
+        }
+        else {
+            parsed_number_from_worksheet.push(work_sheet[4][idx -1].to_string());
+        }
+    }
+
+    for item in parsed_number_from_worksheet {
+        println!("{item}");
+    }
+
+    result
+}
+
 fn main() {
     let input = aoc_2025::read_file("input_day06.txt");
     let result_part1 = parse_input_to_work_sheet(&input);
     println!("result part 1 {}", result_part1);
+    let result_part2 = part2(&input);
+    println!("result part 2 {}", result_part2);   
 }
